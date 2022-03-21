1c8735248bae7e32294c0fe4254df393af816dbd
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-21 02:33:00 -0400

    middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,83 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+const vinValidator = require('vin-validator')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (car) {
+      req.car = car
+      next();
+    } else {
+      next({
+        status:404,
+        message: `car with id ${req.params.id} is not found`
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (!req.body.vin) {
+    next({
+      status: 400,
+      message: 'vin is missing'
+    })
+  } else if (!req.body.make) {
+    next({
+      status: 400,
+      message: 'make is missing'
+    })
+  } else if (!req.body.model) {
+    next({
+      status: 400,
+      message: 'model is missing'
+    })
+  } else if (!req.body.mileage) {
+    next({
+      status: 400,
+      message: 'mileage is missing'
+    })
+  } else {
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  const isValidVin = vinValidator.validate(req.body.vin)
+  if (isValidVin === true) {
+    next()
+  } else {
+    next({
+      status: 400,
+      message: `vin ${req.body.vin} is invalid`
+    })
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const notUnique = await Cars.checkVin(req.body.vin)
+  if (notUnique) {
+    next({
+      status: 400,
+      message: `vin ${req.body.vin} already exists`
+    })
+  } else {
+    next()
+  }
+}
+
+const errorHandling = (err, req, res, next) => {//eslint-disable-line
+  res.status(err.status || 500).json({
+    message: `${err.message}`
+  })
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+  errorHandling
 }
