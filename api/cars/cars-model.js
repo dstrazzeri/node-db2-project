c2d890af84768aea04c7c34a987405dd826f3a74
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-21 02:21:31 -0400

    fleshed out model functions in cars model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,28 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config')
+
+const getAll = async () => {
+  const rows = await db('cars')
+  .select('id', 'vin', 'make', 'model', 'mileage', 'title', 'transmission')
+
+  return rows
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  const [car] = await db('cars')
+  .select('id', 'vin', 'make', 'model', 'mileage', 'title', 'transmission')
+  .where('id', '=', id)
+
+  return car
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (car) => {
+  return db('cars')
+  .insert(car)
+  .then(([id]) => getById(id))
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
